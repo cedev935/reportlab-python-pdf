9692cade65c1c499d00f251e970accebbec4f609
cedev935 <cedev935@gmail.com>
2022-01-31 16:25:18 +0200

    generate voucher

--- create-pdf.py
+++ create-pdf.py
@@ -6,84 +6,60 @@ from send_email import EmailController as email_controller
 
 
 def create_pdf(
-    beneficiary_name: str,
-    beneficiary_bank: str,
-    beneficiary_account_number: str,
-    transaction_amount: str,
-    transaction_date: str,
-    transaction_type: str,
-    transaction_reference: str,
-    transaction_purpose: str,
+    amount: str,
+    voucher_number: str,
 ):
     """Create PDF
 
     Args:
-        beneficiary_name: beneficiary_name
-        beneficiary_bank: beneficiary_bank
-        beneficiary_account_number: beneficiary_account_number
-        transaction_amount: transaction_amount
-        transaction_date: transaction_date
-        transaction_type: transaction_type
-        transaction_reference: transaction_reference
-        transaction_purpose: transaction_purpose
+        amount: amount
+        voucher: voucher
 
     """
     packet = io.BytesIO()
 
     can = canvas.Canvas(packet, pagesize=letter)
-    
-    # can.setFillColorRGB(255, 255, 255) #
-    can.drawString(280, 503, beneficiary_name)
-    can.drawString(280, 477, beneficiary_bank)
-    can.drawString(280, 453, beneficiary_account_number)
-    can.drawString(280, 427, transaction_amount)
-    can.drawString(280, 402, transaction_date)
-    can.drawString(280, 377, transaction_type)
-    can.drawString(280, 355, transaction_reference)
-    can.drawString(280, 330, transaction_purpose)
+
+    can.setFillColor("#144f5d")  #
+    can.setFont("Helvetica-Bold", 35)
+    can.drawString(40, 130, amount)
+
+    can.setFillColor("#144f5d")  #
+    can.setFont("Helvetica-Bold", 10)
+    can.drawString(600, 250, voucher_number)
+
+    can.setFillColor("#144f5d")  #
+    can.setFont("Helvetica-Bold", 20)
+    can.drawString(300, 120, voucher_number)
     can.save()
 
     packet.seek(0)
     new_pdf = PdfFileReader(packet)
     # getting the existing pdf file
-    existing_pdf = PdfFileReader(open("assets/original.pdf", "rb"))
+    existing_pdf = PdfFileReader(open("assets/voucher.pdf", "rb"))
     output = PdfFileWriter()
 
-    # add our text to the pdf 
+    # add our text to the pdf
     page = existing_pdf.getPage(0)
     page.mergePage(new_pdf.getPage(0))
     output.addPage(page)
 
     # genertate a new pdf
-    outputStream = open("generated-pdf/" + transaction_reference + ".pdf", "wb")
+    outputStream = open("generated-pdf/" + voucher_number + ".pdf", "wb")
     output.write(outputStream)
     outputStream.close()
 
-    email_controller.send_pop_as_email(transaction_reference)
+    # email_controller.send_pop_as_email(transaction_reference)
 
 
-
-beneficiary_name = "Schweppes"
-beneficiary_bank = "Steward Bank"
-beneficiary_account_number = "1003088252"
-transaction_amount = "$ 110898.58"
-transaction_date = "30-04-2021"
-transaction_type = "INTERNET RTGS"
-transaction_reference = "413110430H000274"
-transaction_purpose = "null"
-
+amount = "$USD50"
+voucher_number = "EW5144522885505575112"
 
 # i = 0
 # while i < 100:
 create_pdf(
-    beneficiary_name=beneficiary_name,
-    beneficiary_bank=beneficiary_bank,
-    beneficiary_account_number=beneficiary_account_number,
-    transaction_amount=transaction_amount,
-    transaction_date=transaction_date,
-    transaction_type=transaction_type,
-    transaction_reference=transaction_reference,
-    transaction_purpose=transaction_purpose,
+    amount=amount,
+    voucher_number=voucher_number,
 )
 
-    # i += 1
+# i += 1
